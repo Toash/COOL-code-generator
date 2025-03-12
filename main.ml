75c3fb8036a4214ea9eda01026ca84e4e312d301
Justin <59674603+Toash@users.noreply.github.com>
2025-03-12 13:38:14 -0400

    handling more expression types

--- main.ml
+++ main.ml
@@ -23,20 +23,34 @@ and formal = id * cool_type
 and binding = id * cool_type * (exp option) 
 and case_element = id * cool_type * exp 
 and exp = 
-  | AST_Variable of string
-  | AST_Int of string 
+  | AST_Block of exp list
+ 
+  | AST_New of cool_type
+  | AST_Isvoid of exp
+
   | AST_Plus of exp * exp
+  | AST_Minus of exp * exp
   | AST_Times of exp * exp
+  | AST_Divide of exp * exp
+
+  | AST_Not of exp
+  | AST_Negate of exp
+
+  | AST_Int of string 
+  | AST_Variable of string
 and
 (* what we will output *)
 tac_instr = 
 | TAC_Assign_Int of string * string (* x <- int 1 *) 
 | TAC_Assign_Plus of string * tac_expr * tac_expr (* x <- + 1 2 *)
+| TAC_Assign_Minus of string * tac_expr * tac_expr (* x <- - 1 2 *)
+| TAC_Assign_Times of string * tac_expr * tac_expr (* x <- * 1 2 *)
+| TAC_Assign_Divide of string * tac_expr * tac_expr (* x <- / 1 2 *)
 and
 tac_expr =
 | TAC_Variable of string
 
-let counter = ref 0 
+let counter = ref 1 
 let main () = begin
   (* 
   when recursive descenting and finding variable,
@@ -50,23 +64,56 @@ let main () = begin
   in
 
   (*
-  convert statements in ast to TAC instructions
+  convert expressions in ast to TAC instructions
 
   return list of instructions and
   return variable 
   *)
   let rec convert ast = begin
     match ast with
+    | AST_Block(v) -> 
+      (* 
+      accumualte instructions for each instruction in the block,
+       return last expression
+        *)
+      let rec convert_block lst =
+        match lst with
+        | [] -> failwith "empty block"
+        | [x] -> convert x
+        | hd::tl -> 
+          let i1, t1 = convert hd in
+          let i2, t2 = convert_block tl in
+          i1@i2, t2
+      in
+      convert_block v
     | AST_Variable (v) -> [], TAC_Variable(v)
-    | AST_Int (i) -> 
+    | AST_Int (v) -> 
       let new_var = fresh_var () in
-      [TAC_Assign_Int (new_var, i)], TAC_Variable(new_var)
+      [TAC_Assign_Int (new_var, v)], TAC_Variable(new_var)
     | AST_Plus(a1,a2) ->
       let i1, ta1 = convert a1 in
       let i2, ta2 = convert a2 in
       let new_var = fresh_var () in
       let to_output = TAC_Assign_Plus(new_var, ta1, ta2) in
-      (i1 @ i2 @ [to_output]), TAC_Variable(new_var)
+      (i1@i2@[to_output]), TAC_Variable(new_var)
+    | AST_Minus(a1,a2) ->
+      let i1, ta1 = convert a1 in
+      let i2, ta2 = convert a2 in
+      let new_var = fresh_var () in
+      let to_output = TAC_Assign_Minus(new_var, ta1, ta2) in
+      (i1@i2@[to_output]), TAC_Variable(new_var)
+    | AST_Times(a1,a2) ->
+      let i1, ta1 = convert a1 in
+      let i2, ta2 = convert a2 in
+      let new_var = fresh_var () in
+      let to_output = TAC_Assign_Times(new_var, ta1, ta2) in
+      (i1@i2@[to_output]), TAC_Variable(new_var)
+    | AST_Divide(a1,a2) ->
+      let i1, ta1 = convert a1 in
+      let i2, ta2 = convert a2 in
+      let new_var = fresh_var () in
+      let to_output = TAC_Assign_Divide(new_var, ta1, ta2) in
+      (i1@i2@[to_output]), TAC_Variable(new_var)
     | _ -> failwith "not implemented"
   
     end in
@@ -161,6 +208,21 @@ let main () = begin
       let exp1 = read_exp() in
       let exp2 = read_exp() in
       AST_Plus(exp1,exp2)
+    | "minus" -> 
+      (* printf "reading plus\n"; *)
+      let exp1 = read_exp() in
+      let exp2 = read_exp() in
+      AST_Minus(exp1,exp2)
+    | "times" -> 
+      (* printf "reading plus\n"; *)
+      let exp1 = read_exp() in
+      let exp2 = read_exp() in
+      AST_Times(exp1,exp2)
+    | "divide" -> 
+      (* printf "reading plus\n"; *)
+      let exp1 = read_exp() in
+      let exp2 = read_exp() in
+      AST_Divide(exp1,exp2)
     | "integer" ->
       let ival=read() in 
       (* printf "reading integer %s\n" ival; *)
@@ -169,6 +231,8 @@ let main () = begin
       let _,ident = read_id () in
       (* printf "reading identifier %s\n" ident; *)
       AST_Variable(ident)
+    | "block" ->
+      AST_Block(read_list read_exp)
     | x -> (
       printf "expression kind unhandled: %s" x;
       exit 1
@@ -185,26 +249,30 @@ let main () = begin
         fprintf fout "%s <- int %s\n" v i
     | TAC_Assign_Plus (v, e1, e2) -> 
         fprintf fout "%s <- + %s %s\n" v (match e1 with TAC_Variable(v) -> v) (match e2 with TAC_Variable(v) -> v)
+    | TAC_Assign_Minus(v, e1, e2) -> 
+        fprintf fout "%s <- - %s %s\n" v (match e1 with TAC_Variable(v) -> v) (match e2 with TAC_Variable(v) -> v)
+    | TAC_Assign_Times(v, e1, e2) -> 
+        fprintf fout "%s <- * %s %s\n" v (match e1 with TAC_Variable(v) -> v) (match e2 with TAC_Variable(v) -> v)
+    | TAC_Assign_Divide(v, e1, e2) -> 
+        fprintf fout "%s <- / %s %s\n" v (match e1 with TAC_Variable(v) -> v) (match e2 with TAC_Variable(v) -> v)
     ) ) instructions;
     fprintf fout "return %s\n" (match expression with TAC_Variable(v) -> v); 
   in
   (* go through ast and print TAC *)
   let ast = read_aast() in
+  fprintf fout "comment start\n";
   List.iter (fun x -> (match x
   with
   | ((_,cname), _ , features) -> 
-      (*
-        add exp to list
-      *)
       List.iter(fun x -> (match x
       with
       | Attribute(fname, ftype, Some(finit)) -> 
-        fprintf fout "attribute, need to handle later\n";
+        (* fprintf fout "attribute, need to handle later\n"; *)
         ()
       | Method((_,mname), formals, mtype, mbody) -> 
-        fprintf fout "comment start\n";
         fprintf fout "label %s_%s_0\n" cname mname;
         (* convert method body to TAC *)
+        let _ = counter := 1 in
         let instructions, expression = convert mbody in
         print_tac instructions expression;
       | _ -> ()
@@ -212,7 +280,7 @@ let main () = begin
       
   ) ) ast;
 
-  
+  printf "%s\n" out;
 
   close_out fout;
   
