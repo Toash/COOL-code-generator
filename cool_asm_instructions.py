ac34a6491eb226d8da578d58a73b7807f385cb52
Justin <59674603+Toash@users.noreply.github.com>
2025-04-01 11:26:48 -0400

    updates

---
+++ cool_asm_instructions.py
@@ -0,0 +1,43 @@
+from collections import namedtuple
+
+
+
+# https://kelloggm.github.io/martinjkellogg.com/teaching/cs485-sp25/crm/modern/Cool%20Assembly%20Language.html
+ASM_Comment = namedtuple("ASM_Comment", "comment")
+ASM_Label = namedtuple("ASM_Label", "label")
+
+ASM_Li = namedtuple("ASM_Li", "reg imm") # li r1 <- 123
+ASM_Mov = namedtuple("ASM_Mov", "dest src") # mov r1 <- r2
+
+ASM_Add = namedtuple("ASM_Add", "dst left right")
+ASM_Sub = namedtuple("ASM_Sub", "dst left right")
+ASM_Mul = namedtuple("ASM_Mul", "dst left right")
+ASM_Div = namedtuple("ASM_Div", "dst left right")
+
+ASM_Jmp = namedtuple("ASM_Jmp", "label")
+ASM_Bz = namedtuple("ASM_Bz", "reg label")
+ASM_Bnz = namedtuple("ASM_Bnz", "reg label")
+ASM_Beq = namedtuple("ASM_Beq", "left right label")
+ASM_Blt = namedtuple("ASM_Blt", "left right label")
+ASM_Ble = namedtuple("ASM_Ble", "left right label")
+
+ASM_Call_Label = namedtuple("ASM_Call_Label", "label")
+ASM_Call_Reg = namedtuple("ASM_Call_Reg", "reg")#jump to address stored in register
+ASM_Return = namedtuple("ASM_Return", "") # go to address in ra register (we handle popping ourself)
+
+ASM_Push = namedtuple("ASM_Push", "reg")
+ASM_Pop = namedtuple("ASM_Pop", "reg")
+
+# Memory instructions
+ASM_Ld = namedtuple("ASM_Ld", "dest src offset") # dest <- src[offset]
+ASM_St = namedtuple("ASM_St", "dest src offset") # dest[offset] <- src
+
+ASM_La = namedtuple("ASM_La", "reg label") # load address in register
+
+# alloc r1 r2 allocates memory by amount in r2 and stores pointer in r1.
+ASM_Alloc = namedtuple("ASM_Alloc", "dest src")
+ASM_Constant_integer = namedtuple("ASM_Constant_integer", "int")
+ASM_Constant_raw_string = namedtuple("ASM_Constant_raw_string", "string")
+ASM_Constant_label = namedtuple("ASM_Constant_label", "label")
+
+ASM_Syscall = namedtuple("ASM_Syscall", "name")
--- x86_gen.py
+++ x86_gen.py
@@ -1,12 +1,75 @@
 from cool_asm_gen import CoolAsmGen
+from cool_asm_instructions import *
 import sys
 from pprint import pprint
 
+
+# given cl-type, parses cl-type, converts to cool-asm, then to x86.
 class X86Gen:
     def __init__(self, file):
+        outfile_name = "my_output.s"
         cool_asm_gen = CoolAsmGen(file)
-        pprint(cool_asm_gen.get_asm())
+        # pprint(cool_asm_gen.get_asm())
 
+        try:
+            self.outfile = open(outfile_name,"w")
+            self.cool_asm_to_x86(cool_asm_gen.get_asm())
+        finally:
+            self.outfile.close()
+
+    # I guess we dont need named tuples for x86?
+    def cool_asm_to_x86(self,cool_asm):
+        for instr in cool_asm:
+            match instr:
+                case ASM_Label(label):
+                    self.outfile.write(f"{label}:\n")
+
+                case ASM_Li(reg,imm):
+                    pass
+                case ASM_Mov(dest,src):
+                    pass
+
+                case ASM_Add(dst,left,right):
+                    pass
+
+                case ASM_Call_Label(label):
+                    pass
+                case ASM_Call_Reg(reg):
+                    pass
+                case ASM_Return():
+                    self.tab()
+                    # we need to handle returns differently.
+                    # in cool_asm, return just jumps to ra.
+                    # in x86, it pops from the top of the stack and jumps to that average
+                    self.outfile.write("ret")
+
+                case ASM_Push(reg):
+                    pass
+                case ASM_Pop(reg):
+                    pass
+
+                case ASM_Ld(dest,src,offset):
+                    pass
+                case ASM_St(dest,src,offset):
+                    pass
+
+                case ASM_Alloc(dest,src):
+                    pass
+
+                case ASM_Constant_label(label):
+                    pass
+
+                case ASM_Syscall(name):
+                    #depending on name, call the respective function.
+                    pass
+                case ASM_Constant_label(label):
+                    self.tab()
+                    self.outfile.write(f".quad {label}\n")
+
+                case _:
+                    print("x86: Unhandled Cool_asm:",instr)
+    def tab(self):
+        self.outfile.write("\t\t\t\t\t")
 
 
 if __name__ == "__main__":
