b51b066c8b620a068565907b5ded3dd1bbadf567
Justin <59674603+Toash@users.noreply.github.com>
2025-05-24 02:48:23 -0400

    start semantic

---
+++ semantic/test.py
@@ -0,0 +1,50 @@
+import os
+import subprocess
+import difflib
+
+semantic= "semantic.py"
+input_dir = "./tests"
+
+for fname in os.listdir(input_dir):
+    if fname.endswith(".cl"):
+        input_cl = os.path.join(input_dir,fname)
+
+        subprocess.run(["python3", "../scanner/scanner.py",input_cl])
+        input_lex = os.path.join(input_dir,fname+"-lex")
+
+        subprocess.run(["python3", "../parser/parser.py",input_lex])
+        input_parse= os.path.join(input_dir,fname+"-ast")
+
+        input_type= os.path.join(input_dir,fname+"-type")
+        reference_type= input_cl[:-3] + "-reference.cl-type"
+
+
+        subprocess.run(["python3",semantic,input_parse])
+        subprocess.run(["cool",input_lex,"--type","--out",reference_type[:-8]])
+        
+        with open(input_type, "r") as f1, open(reference_type, "r") as f2:
+            output1 = f1.read()
+            output2 = f2.read()
+        if output1 == output2:
+            print(input_type+ " ✅")
+        else:
+            print(input_type+ " ❌")
+            diff = difflib.unified_diff(
+                output2.splitlines(),  
+                output1.splitlines(),  
+                fromfile=reference_type,
+                tofile=input_type,
+                lineterm=''
+            )
+            for line in diff:
+                print(line)
+
+        subprocess.run(["rm",input_lex])
+        subprocess.run(["rm",input_parse])
+        subprocess.run(["rm",input_type])
+        subprocess.run(["rm",reference_type])
+        
+        
+
+        
+        
